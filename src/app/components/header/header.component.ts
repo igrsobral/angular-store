cd5f7744032fa3dfa2903e2d1463260b0a5a4901
Igor Ribeiro Sobral <igorsobral@brq.com>
2023-01-31 23:27:53 -0300

    feat: add header and material modules

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatMenuPanel } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styles: [
+  ]
+})
+export class HeaderComponent {
+  menu!: MatMenuPanel<any> | null;
+
+}
